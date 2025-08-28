b39a17a563cc6b697b1d599a6f181efdd189ecb2
AnjaneyaGoyal <goyalanjaneya@gmail.com>
2025-08-28 14:48:10 +0530

    Update A3Q5.cpp

--- A3Q5.cpp
+++ A3Q5.cpp
@@ -4,28 +4,4 @@ using namespace std;
 
 int evaluatePostfix(string exp) {
     stack<int> s;
-    for (char c : exp) {
-        if (isdigit(c)) {
-            s.push(c - '0');
-        } else {
-            int val2 = s.top(); s.pop();
-            int val1 = s.top(); s.pop();
-            switch (c) {
-                case '+': s.push(val1 + val2); break;
-                case '-': s.push(val1 - val2); break;
-                case '*': s.push(val1 * val2); break;
-                case '/': s.push(val1 / val2); break;
-            }
-        }
-    }
-    return s.top();
-}
-
-int main() {
-    string postfix;
-    cout << "Enter postfix expression: ";
-    cin >> postfix;
-
-    cout << "Evaluation result: " << evaluatePostfix(postfix) << endl;
-    return 0;
-}
+   
